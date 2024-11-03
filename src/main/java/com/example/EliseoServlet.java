29b09419bbf61276ab1c0182238a3b2a37f375c4
eliseo-lopez-bravo <eliseo.lopez.007@gmail.com>
2024-11-02 22:55:33 -0700

    updated java file

--- src/main/java/com/example/EliseoServlet.java
+++ src/main/java/com/example/EliseoServlet.java
@@ -10,7 +10,7 @@ public class EliseoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello, Petochita!</h1>");
+        response.getWriter().println("<h1>Hello, Eliseo!</h1>");
     }
 }
 
